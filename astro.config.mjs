83e724d82a6e408c24a3762465ffbda74aa14c58
rsstst <rsstst.ann@gmail.com>
2026-05-17 16:13:17 +0700

    update: deploy gh-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import swup from '@swup/astro';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://rsstst.github.io',
   trailingSlash: 'never',
   vite: {
     plugins: [tailwindcss()]
